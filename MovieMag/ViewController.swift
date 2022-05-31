e67d0ca79fe3877120d949c02c89cac48d8ad9fa
Ertugrul Ozvardar <ertugrul.ozvardar@obss.com.tr>
2022-05-31 23:28:41 +0300

    Test commit

--- MovieMag/ViewController.swift
+++ MovieMag/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Test")
         // Do any additional setup after loading the view.
     }
 
